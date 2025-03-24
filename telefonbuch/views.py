d14eb65a297c97ae1e2a028ef5ba04c050beb742
Wild Andreas <andreas.wild@a-sh.de>
2025-03-24 07:30:27 +0100

    First commit

---
+++ telefonbuch/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import TelefonbuchEintrag
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("Hello World!")
+
+def einträge (reguest):
+    einträge = TelefonbuchEintrag.objects.all()
+    return render(reguest , 'einträge.html', context = {'einträge' : einträge})
